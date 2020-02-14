7c2f71857a0541dd77820f0b526d75c321c1e272
cfuj <cfujiyoshi@gmail.com>
2020-02-13 21:37:12 -0800

    Refactored how question is chosen

--- tweet_draft.py
+++ tweet_draft.py
@@ -4,6 +4,8 @@ import random
 with open('black_cards.json') as f:
   black_cards = json.load(f)
 
+# finds the total number of questions in the black cards json
+total_questions = len(black_cards["blackCards"])
+
 # Works to choose a string from the black cards json
-# 67 is the number of black card questions, should un-hardcode the number lol
-question = (black_cards["blackCards"][random.randrange(67)]["text"])
+question = (black_cards["blackCards"][random.randrange(total_questions)]["text"])
