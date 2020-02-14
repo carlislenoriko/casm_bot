4002cb62d278b796d932c493718e721c0489b76e
cfuj <cfujiyoshi@gmail.com>
2020-02-13 22:02:36 -0800

    More black cards and adjusting card choice so I can read card pairs to kill time when I hit a mental block

--- card_choice.py
+++ card_choice.py
@@ -7,8 +7,10 @@ with open('black_cards.json') as f:
 with open('white_cards.json') as boop:
 	white_cards = json.load(boop)
 
+total_questions = len(black_cards["blackCards"])
+
 # Works to choose a string from the black cards json
-print(black_cards["blackCards"][random.randrange(67)]["text"])
+print(black_cards["blackCards"][random.randrange(total_questions)]["text"])
 
 # Works to choose a string from the white cards json
 print(random.choice(white_cards["whiteCards"]))
