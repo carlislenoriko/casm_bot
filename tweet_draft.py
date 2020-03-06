26e80b30afb4ba0b8300509baa9d81339459f21c
cfuj <cfujiyoshi@gmail.com>
2020-03-05 18:45:23 -0800

    Added Twitter auth stuff

--- tweet_draft.py
+++ tweet_draft.py
@@ -1,5 +1,12 @@
 import json
 import random
+import tweepy
+import secrets
+
+# twitter auth stuff
+auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+api = tweepy.API(auth)
 
 with open('black_cards.json') as f:
   black_cards = json.load(f)
